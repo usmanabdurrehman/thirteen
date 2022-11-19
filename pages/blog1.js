863cf1667817f607ca9ef06bc6beab477056ccb9
Usman Abdur Rehman <saidskit@outlook.com>
2022-11-20 00:46:41 +0500

    Added blog1 page

---
+++ pages/blog1.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Blog1() {
+  return (
+    <div>
+      <Head>
+        <title>Technological Advancement</title>
+        <meta
+          property="og:image"
+          content="https://thirteen-seven.vercel.app/api/og?title=Technological+Advancement"
+        />
+      </Head>
+    </div>
+  );
+}
